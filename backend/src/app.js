0a87f12377ed384815e216ec0caad4b8b2b6ad8c
Anderson Fabiano Dums <af.dums@gmail.com>
2026-01-22 22:53:37 -0300

    Criação do CORS para acesso do FrontEnd ao backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,19 @@
+const cors = require("cors");
 const express = require("express");
 const { authRouter } = require("./modules/auth/auth.routes");
 const { healthRouter } = require("./modules/health/health.routes");
 
 const app = express();
 
+const allowedOrigins = process.env.FRONTEND_URL
+  ? [process.env.FRONTEND_URL]
+  : ["http://localhost:5173"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
 app.use(express.json());
 
 app.use("/auth", authRouter);
